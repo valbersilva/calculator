13405a32b1958de3040c9a2a4f067c051585e086
valberlpk <81580195+valberlpk@users.noreply.github.com>
2021-04-06 19:25:59 -0300

    up1

---
+++ app.js
@@ -0,0 +1,20 @@
+var iniciar = parseInt(alert("Qual operação voce deseja?"));
+var escolherOperação = parseInt(prompt("1-Soma, 2-Subtração, 3-Multiplicação, 4-Divisão"));
+
+var primeiroValor = parseInt(prompt("Qual seu primeiro valor?"));
+var segundoValor = parseInt(prompt("Qual seu segundo valor?"));
+
+
+if (escolherOperação === 1) {
+    //  block of code to be executed if the condition is true
+  } else {
+    if (escolherOperação === 2) {
+        //  block of code to be executed if the condition is true
+      } else {
+        if (escolherOperação === 3) {
+            //  block of code to be executed if the condition is true
+          } else {
+            //  block of code to be executed if the condition is false
+          }
+      }
+  }
